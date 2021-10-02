fefca4e7fa4ac1d15284870c3e8fb3cbbfc30d8a
furriest <valery@pluto.pw>
2021-10-02 15:59:55 +0000

    publish main code

---
+++ suri.py
@@ -0,0 +1,31 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+
+TOKEN = "/opt/suricata/token.key"
+
+with open(TOKEN, 'r') as file:
+    BOT_TOKEN = file.read().replace('\n', '')
+
+async def start_handler(event: types.Message):
+    await event.answer(
+        f"Hello, {event.from_user.get_mention(as_html=True)} 👋!",
+        parse_mode=types.ParseMode.HTML,
+    )
+
+async def suri_handler(event: types.Message):
+    await event.answer(
+        f"Hello, suri {event.from_user.get_mention(as_html=True)} 👋!",
+        parse_mode=types.ParseMode.HTML,
+    )
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    try:
+        disp = Dispatcher(bot=bot)
+        disp.register_message_handler(start_handler, commands={"start", "restart"})
+        disp.register_message_handler(suri_handler, commands={"suri"})
+        await disp.start_polling()
+    finally:
+        await bot.close()
+
+asyncio.run(main())
